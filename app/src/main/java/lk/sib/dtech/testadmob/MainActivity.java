556b1dc1930cc95cd36a9e0a1c3c34d5cd5a997f
shashilaheshan <ebayshashila@gmail.com>
2019-01-07 12:41:17 +0530

    a

---
+++ app/src/main/java/lk/sib/dtech/testadmob/MainActivity.java
@@ -0,0 +1,24 @@
+package lk.sib.dtech.testadmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this,
+                "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
